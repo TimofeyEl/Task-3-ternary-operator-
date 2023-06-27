a3c7722ba88568060058fe5b63a0a36e401ea604
User <User@DESKTOP-4ROMB9B>
2023-06-27 16:12:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__3_ternary_operator_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            int userNumber;
+            Console.WriteLine("Введите число: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            while (n <= userNumber - 1) 
+            {
+                Console.WriteLine(userNumber % n == 0 ? "Число является составным." :userNumber == 2? "Число является простым.":"Число является простым");
+                break;
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
